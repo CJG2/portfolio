cc4353d52762622d6ef249c61c4b0af11cc5ce17
Guillermo <carlos.GUILLERMO-GOMEZ@etu.univ-amu.fr>
2024-12-05 11:53:10 +0100

    Réglage de certains pbs et ajout d'un projet

--- style/javascript/script.js
+++ style/javascript/script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
             
-    var copy1 = document.getElementById('copier1');
-    var copy2 = document.getElementById('copier2');
-
-    copy1.addEventListener('click', function() {
+    
+    const numeroTelephone = document.querySelector("#numeroTelephone");
+    var copy1 = document.querySelector("#copier1");
+    numeroTelephone.addEventListener('click', () => {
         copierTexteDansPressePapier(copy1.textContent);
     });
-    
-    copy2.addEventListener('click', function() {
+
+    const mailPerso = document.querySelector("#mailPerso");
+    var copy2 = document.querySelector("#copier2");
+    mailPerso.addEventListener('click', () => {
         copierTexteDansPressePapier(copy2.textContent);
+    });
+
+    const instagramPerso = document.querySelector("#instagramPerso");
+    instagramPerso.addEventListener('click', () => {
+        window.open('https://www.instagram.com/cguillermo18/', '_blank');
     });
     
     function copierTexteDansPressePapier(texte) {
@@ -50,4 +57,5 @@ document.addEventListener('DOMContentLoaded', function() {
         var hue = niveau * 120; 
         return 'hsl(' + hue + ', 100%, 50%)'; 
     }
+
 });
